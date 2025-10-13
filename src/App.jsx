129eb3da8cc597aaf01e8ebd472a11ea39d4a67e
Iury Rodrigues de Godoy <iuryrodriguesgodoy18@gmail.com>
2025-10-13 10:14:05 -0300

    Normalize filename

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import "./App.css";
-import Menu from "./components/Menu/Menu";
-import About from "./components/About/About";
-import Service from "./components/Services-appointments/Service";
-import EspacoSection from "./components/Services-appointments/EspacoSection";
-import Products from "./components/Products/Products";
-import Faq from "./components/Faq/Faq";
+import './App.css';
+import Menu from './components/Menu/Menu';
+import About from './components/About/About';
+import Service from './components/Services-appointments/Service';
+import EspacoSection from './components/Services-appointments/EspacoSection';
+import Products from './components/Products/Products';
+import Faq from './components/Faq/Faq';
 
 function App() {
   return (
