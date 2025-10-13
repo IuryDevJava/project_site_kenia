b0eca3d612d3fce680ff94cacd84bc1abe420caa
Iury Rodrigues de Godoy <iuryrodriguesgodoy18@gmail.com>
2025-10-13 10:09:36 -0300

    Normalize filename cases

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import "./App.css";
-import Menu from './components/Menu/Menu';
-import About from './components/About/About';
-import Service from './components/Services-appointments/Service';
-import EspacoSection from './components/Services-appointments/EspacoSection';
-import Products from './components/Products/Products';
-import FAQ from './components/Faq/Faq';
-
-
+import Menu from "./components/Menu/Menu";
+import About from "./components/About/About";
+import Service from "./components/Services-appointments/Service";
+import EspacoSection from "./components/Services-appointments/EspacoSection";
+import Products from "./components/Products/Products";
+import Faq from "./components/Faq/Faq";
 
 function App() {
   return (
     <div className="app">
       <Menu />
       <About />
-      <Service/>
+      <Service />
       <EspacoSection />
       <Products />
-      <FAQ />
+      <Faq />
     </div>
   );
 }
